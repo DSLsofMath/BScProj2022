c236c421467e13915eebd683d492a40777d614a8
AdamEliasson <eladam@student.chalmers.se>
2022-03-05 11:45:19 +0100

    Create Eigen.hs

---
+++ src/Eigen.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeApplications #-}
+
+
+module Eigen where
+
+import GHC.TypeLits
+import qualified Prelude
+import Prelude hiding ((+), (-), (*), (/), recip, sum, product, (**), span)
+
+import qualified Data.List as L
+import Algebra
+import ListVector hiding (v1,v2,m1,m2)
+
+-- This file contains an example of using TypeLits to handle vectors with a given size. 
+-- The implementation here is based on lists and should be replaced.
+--
+-- To try the code in ghci the DataKinds language extension needs to be enabled:
+-- type ":set -XDataKinds" in the ghci prompt
+--
+
+-- Implementation for 2X2 and 3x3 determinants
+-- seems to work, tested several inputs and compared with wolfram
