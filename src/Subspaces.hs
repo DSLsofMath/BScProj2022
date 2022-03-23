22b73451f272043b9dd2810e23265d56ea0dccce
FilipNordmark <filip00.nordmark@gmail.com>
2022-03-23 18:06:12 +0100

    Added comments on nullSpace and solveQ

--- src/Subspaces.hs
+++ src/Subspaces.hs
@@ -86,19 +86,24 @@ makeLinIndep = foldl (\vs v -> if span' (M $ V vs) v then vs else v:vs) []
 isBasis :: (KnownNat m, KnownNat (n-1), Eq f, Field f) => Matrix f m n -> Bool
 isBasis m = spansSpace m && linIndep m
 
+-- | The null space of a linear transformation is the set of vectors that maps to 0
+--   In terms of linear equations it is the solution to Ax=0
 nullSpace :: (KnownNat n, Field f, Eq f) =>  Matrix f m n -> Subspace (Vector f n)
-nullSpace m = Sub $ map (V . drop height) $ filter (all (==zero) . take height) $ unpack $ transpose $ utf (transpose m `append` idm)
+nullSpace m = Sub $ [ V b | (a,b) <- splitAt height <$> reduceCol m, all (== zero) a]
     where height = length (head $ unpack m)
+          reduceCol m = unpack $ transpose $ utf (transpose m `append` idm)
 
-
+          
+-- | A quotient space is a subspace translated by a vector
 data QuotientSpace v = Quot v (Subspace v) deriving (Show)
 
-
-solveQ :: (KnownNat n, Field f, Eq f, (n ~ (n+1-1)) ) =>  Matrix f m n -> Vector f m -> QuotientSpace (Vector f n)
+-- | Returns the set of solutions to Ax=v
+solveQ :: (KnownNat n, Field f, Eq f, (n ~ (n+1-1)) ) => Matrix f m n -> Vector f m -> QuotientSpace (Vector f n)
 solveQ m v = Quot (solvesys $ m `append'` v) (nullSpace m)
     where append' :: Matrix f m n -> Vector f m -> Matrix f m (n+1)
           m `append'` v = m `append` M (V [v]) 
 
+-- | Equivalent to solveQ but takes a matrix A `append` v representing Ax=v
 solveQ' :: (KnownNat n, Field f, Eq f, (n ~ (n+1-1)) ) =>  Matrix f m (n+1)  -> QuotientSpace (Vector f n)
 solveQ' m = let (v', m') = last $ separateCols m in solveQ m' v'
 
