46aa94b6e482ddee76c30b6996d8f555f04e4b1f
FilipNordmark <filip00.nordmark@gmail.com>
2022-03-30 01:18:39 +0200

    Tried to convert Quad to Matrix

--- src/QuadTree.hs
+++ src/QuadTree.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoStarIsType #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module QuadTree where
 
@@ -60,36 +61,65 @@ instance (Num a) => Num (Matrx a) where
 
 -}
 
--- Reprsents a matrix size n*n where n = 4^Nat4
-data Nat4 = Nil | Succ Nat4
+data Nat4 = One | Suc Nat4
 
 type family Pred (n :: Nat4) :: Nat4 where
-     Pred (Succ n)  = n
+     Pred (Suc n)  = n
 
 type family ToNat (n :: Nat4) :: Nat where
-     ToNat Nil = 1
-     ToNat (Succ n)  = 4 * ToNat n
+     ToNat One = 1
+     ToNat (Suc n)  = 2 * ToNat n
 
 
--- type Quad :: Nat4 -> Type -> Type
+-- Represents a matrix of size n*n where n = 2^Nat4
 data Quad (n :: Nat4) a where 
     Zero :: Quad n a
-    Scalar :: a -> Quad Nil a
+    Scalar :: a -> Quad One a
     Mtx :: {
-        nw :: Quad (Pred n) a,
-        ne :: Quad (Pred n) a,
-        sw :: Quad (Pred n) a, 
-        se :: Quad (Pred n) a 
-           } -> Quad n a 
-    Id :: Quad n a --used mostly in permutations.
+        nw :: Quad n a,
+        ne :: Quad n a,
+        sw :: Quad n a, 
+        se :: Quad n a 
+           } -> Quad (Suc n) a 
+    -- Id :: Quad n a --used mostly in permutations.
+
+
+-- | Applies a function on the Quads scalars
+mapQuad :: (a -> b) -> Quad n a -> Quad n b
+mapQuad f Zero = Zero
+mapQuad f (Scalar s) = Scalar (f s)
+mapQuad f (Mtx nw ne sw se) = Mtx (mapQuad f nw) (mapQuad f ne) 
+                                  (mapQuad f sw) (mapQuad f se)
+
+instance Functor (Quad n) where
+   fmap = mapQuad 
 
 instance AddGroup a => AddGroup (Quad n a) where
     Zero + x    = x
     x    + Zero = x
-    Scalar a    + Scalar b = Scalar (a+b)
-    Mtx nw1 ne1 sw1 se1 + Mtx nw2 ne2 sw2 se2 = Mtx (nw1+nw2) (nw1+nw2) (nw1+nw2) (nw1+nw2)
-    a - b = undefined
+    Scalar a + Scalar b = Scalar (a+b)
+    Mtx nw1 ne1 sw1 se1 + Mtx nw2 ne2 sw2 se2 = Mtx (nw1+nw2) (nw1+nw2) 
+                                                    (nw1+nw2) (nw1+nw2)
+    neg = fmap neg
     zero = Zero 
+
+instance Ring a => VectorSpace (Quad n a) where
+    type Under (Quad n a) = a
+    s £ q = fmap (s*) q
+
+
+-- | Converts a Quad matrix into a list of lists matrix
+--
+--   TODO: zero requires a KnownNat constraint but we cannot deduce 
+--   KnownNat in the recursive call even though ToNat is trivial.
+--   We also should not need to (pack . unpack) but it fails to
+--   deduce n + n = 2 * n
+toDense :: (Ring a) => Quad n a -> Matrix a (ToNat n) (ToNat n) 
+-- toDense Zero = zero
+toDense (Scalar a) = a £ idm    -- Will always be of size 1x1
+toDense (Mtx nw ne sw se) = pack . unpack $ (toDense nw `append` toDense ne) 
+                                  `append'` (toDense sw `append` toDense se)
+
 
 
 -- Potentally use parallelism for big enough Quads
