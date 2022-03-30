d2f8b2ad9f378ee547414beb405f1ed91dc5e6c2
sebastiansjogrn <sebastiansjogrn@gmail.com>
2022-03-30 15:58:07 +0200

    Addded Addition and Subtraction between CSR sparse matrixes.

--- src/SparseCSR.hs
+++ src/SparseCSR.hs
@@ -15,12 +15,20 @@ import Algebra
 import Prelude hiding ((+), (-), (*), (/), sum)
 import ListVector 
 
+import Data.List hiding (sum)
+
 -- A sparse matrix using the compressed sparse row format
 data CSR f (m :: Nat) (n :: Nat) = CSR { elems :: [f],
                             col :: [Int],
                             row :: [Int]}
 
 deriving instance Show f => Show (CSR f m n)
+
+instance AddGroup f => AddGroup (CSR f m n) where
+    (+) = cSRAdd
+    (-) = cSRSub
+    neg (CSR e c r) = CSR (map neg e) c r 
+    zero = CSR [] [] [0,0] -- should be [0,size+1, info from type] 
 
 instance (KnownNat m, KnownNat n, AddGroup f) => ToMat m n (CSR f m n) where
     type Under' (CSR f m n) = f
@@ -49,10 +57,36 @@ dotL v1 v2 = sum $ zipWith (*) v1 v2
 -- Matrix operations
 --  
 
+cSRSub :: AddGroup f => CSR f a b -> CSR f a b  -> CSR f a b
+cSRSub m1 (CSR e c r) = cSRAdd  m1 (CSR (map neg e) c r)
+
+cSRAdd :: AddGroup f => CSR f a b -> CSR f a b  -> CSR f a b
+cSRAdd (CSR e1 c1 [r]) _ = CSR [] [] [r]
+cSRAdd (CSR e1 c1 (r1:rw1)) 
+       (CSR e2 c2 (r2:rw2)) = CSR e c [r1] `cSRcombine` cSRAdd (CSR (drop j1 e1) (drop j1 c1) (map (+(length e - j1)) rw1)) (CSR (drop j2 e2) (drop j2 c2) rw2)
+       where (c,e) = cSRAddRow (take j1 c1,take j1 e1) (take j2 c2,take j2 e2)
+             j1 = head rw1 - r1
+             j2 = head rw2 - r2
+
+-- Adds two rows of a csr matrix
+cSRAddRow :: AddGroup f => ([Int], [f]) -> ([Int], [f])  -> ([Int], [f])
+cSRAddRow (_,[]) (_,[]) = ([],[])
+cSRAddRow (_,[]) (cs,es) = (cs,es)
+cSRAddRow (cs, es) (_,[]) = (cs, es)
+cSRAddRow (c1:cs1,e1:es1) 
+           (c2:cs2,e2:es2) | c1 == c2 = ([c1],[e1 + e2]) `addT` cSRAddRow (cs1,es1) (cs2,es2)
+                           | c1 > c2  = ([c2,c1],[e2,e1]) `addT` cSRAddRow (cs1,es1) (cs2,es2)
+                           | c1 < c2  = ([c1,c2],[e1,e2]) `addT` cSRAddRow (cs1,es1) (cs2,es2)
+
+cSRcombine :: AddGroup f => CSR f a b -> CSR f a b  -> CSR f a b
+cSRcombine (CSR e1 c1 r1) (CSR e2 c2 r2) = CSR (e1++e2) (c1++c2) (r1++r2)
+
+addT :: AddGroup f => ([Int], [f]) -> ([Int], [f])  -> ([Int], [f])
+addT (cs1, es1) (cs2, es2) = (cs1++cs2, es1++es2)                                   
+
 -- Matrix Vector Multiplication                                                                                                                                                                                                                                                                                                                                                                       
 
--- Multiplies a CSR matrix with a Vector
--- Might be some problems with precise vector definitions 
+-- Multiplies a CSR matrix with a Vector 
 smV :: (AddGroup f, Mul f) => CSR f a b -> Vector f b  -> Vector f a
 smV m (V v) = V (smv m v)
 
@@ -73,20 +107,12 @@ test = CSR {
     col = [ 0, 1, 2, 1 ],
     row = [ 0, 1, 2, 3, 4 ]}
 
-<<<<<<< HEAD
--- Test Matrix
-test2 :: CSR Int 4 4
-=======
 test2 :: CSR Double 4 4
->>>>>>> 8cfabfbcf3602e0ad5886c0693073364c18b1213
 test2 = CSR {
     elems = [ 5, 4, 8, 3, 6 ],
     col = [ 0, 1, 1, 2, 1 ],
     row = [ 0, 2, 3, 4, 5 ]}
 
-<<<<<<< HEAD
-colVecTest :: CSR Int 4 1
-=======
 test3 :: CSR Double 2 2
 test3 = CSR {
     elems = [ 5, 6 ],
@@ -94,7 +120,6 @@ test3 = CSR {
     row = [ 0, 1, 2]}
 
 colVecTest :: CSR Double 4 1
->>>>>>> 8cfabfbcf3602e0ad5886c0693073364c18b1213
 colVecTest = CSR {
     elems = [4,1,3],
     col = [0, 0, 0],
@@ -109,43 +134,4 @@ v11, v22 :: Vector Double 4
 v11 = V [5,3,1,8]::VecR 4
 v22 = V [8,2,8,3]::VecR 4
 
-<<<<<<< HEAD
-getRow :: CSR f m n -> Int -> ([Int], [f])
-getRow (CSR elems col row) i = case td i 2 row of
-        [a,b] -> unzip $ td a (b-a) (zip col elems) 
-        _     -> ([],[])
-        where td i j = take j . drop i
-
-getColumn :: CSR f m n -> Int -> ([Int], [f])
-getColumn (CSR elems col row) i = undefined 
-
---getColumnIndex i col elems = unzip $ filter zip col elems
---    \(i',_) -> i' = i
-
-dotL :: (AddGroup f, Mul f) => [f] -> [f] -> f
-dotL v1 v2 = sum $ zipWith (*) v1 v2
-
---
--- Matrix operations
---  
-
--- Matrix Vector Multiplication                                                                                                                                                                                                                                                                                                                                                                       
-
-smv :: (AddGroup f, Mul f) => CSR f a b -> [f] -> [f]
-smv (CSR _ _ (r:[])) v = []
-smv (CSR [] _ _) v = []
-smv (CSR elems col (r:row)) v = dotL (take j elems) (listIndex v (take j col)) : 
-                                     smv (CSR (drop j elems) (drop j col) row) v
-            where j = head row - r 
-
-testsmv = smv test [1,1,1,1]  == [5,8,3,6]
-
-testsmv2 = smv test2 [1,1,1,1]  == [9,8,3,6]
-
--- | Take a list uses a list of indexes to print index values
-listIndex :: [f] -> [Int] -> [f]
-listIndex as [] = []
-listIndex as indexes = map (as!!) indexes -- i : listIndex as indexes
-=======
 testsmv3 = smV test v11  
->>>>>>> 8cfabfbcf3602e0ad5886c0693073364c18b1213
