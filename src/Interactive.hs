3c728c195b773074ceda5434fe0d504556077090
Linus S <lsundkvist1@gmail.com>
2022-05-05 16:38:51 +0200

    interactive experience pt2

---
+++ src/Interactive.hs
@@ -0,0 +1,13 @@
+module Interactive where
+
+import Algebra
+import Eigen
+import Gauss
+import Subspaces
+import Matrix hiding (Matrix)
+import QuadTree (QuadM)
+import SparseCSR as S
+import ListVector as L
+
+import Prelude hiding ((+), (-), (*), (/), (^), (**), sum, product, recip, fromRational)
+
